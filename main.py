13192202aa4a1c38b073139ef33c363d83850cf3
Artmklovesleep <potapovartem101@gmail.com>
2023-04-02 21:05:21 +0300

    states

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from aiogram.types import InlineKeyboardButton,  InlineKeyboardMarkup, ReplyKeyb
 import asyncio
 from config import BOT_TOKEN
 import keyboard
+from states import Start
 
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
@@ -24,15 +25,26 @@ async def start_command(message: types.Message):
                                'Добро пожаловать в Get Poison'), reply_markup=keyboard)
 
 
-@dp.message_handler(commands=["Рассчитать стоимость"])
-async def start_command(message: types.Message):
+@dp.message_handler(commands=["Рассчитать стоимость"], state="*")
+async def rasshet(message: types.Message):
     global user_id
     user_id = message.from_user.id
     firstname = message.from_user.first_name
     await bot.send_message(message.from_user.id,
                            text=(
                                'Добро пожаловать в Get Poison. Введите стоимость '))
+    await Start.schet.set()
 
+@dp.message_handler(state=Start.schet)
+async def rasshet_itog (message: types.Message, state: FSMContext) -> None:
+    async with state.proxy() as data:
+        data['stoim'] = message.text
+        stoim_cny = int( data['stoim'])
+        itog = stoim_cny * curs + 1500
+        await bot.send_message(message.from_user.id,
+                            text=(
+                                'Сумма вашего заказа  = ' + str (itog)))
+    await state.finish()
 
 if __name__ == "__main__":
     executor.start_polling(
