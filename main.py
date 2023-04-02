ba232e0a2114f8a7cf9099766e640757481b0be2
Artmklovesleep <potapovartem101@gmail.com>
2023-04-02 22:49:13 +0300

    ed

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import asyncio
 from config import BOT_TOKEN
 from keyboard import keyboard
 from states import Start
+from checker import checker
 
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
@@ -13,7 +14,8 @@ loop = asyncio.get_event_loop()
 
 
 curs = 12.9
-
+nacenka = 1500
+dostavka = 1300
 
 @dp.message_handler(commands=["start"], state="*")
 async def start_command(message: types.Message):
@@ -41,11 +43,18 @@ async def rasshet(message: types.Message):
 async def rasshet_itog(message: types.Message, state: FSMContext) -> None:
     async with state.proxy() as data:
         data['stoim'] = message.text
-        stoim_cny = int(data['stoim'])
-        itog = stoim_cny * curs + 1500
-        await bot.send_message(message.from_user.id,
-                               text=(
-                                   'Сумма вашего заказа  = ' + str(itog)))
+        stoim = data['stoim']
+        s = ''
+        stoim_cny,s = await checker(stoim, s)
+        if stoim_cny == -1:
+            await bot.send_message(message.from_user.id,
+                               text=(s))
+            await Start.schet.set()    
+        else:                       
+            itog = stoim_cny * curs + nacenka + dostavka
+            await bot.send_message(message.from_user.id,
+                                text=(
+                                    'Сумма вашего заказа  = ' + str(itog)))
     await state.finish()
 
 if __name__ == "__main__":
