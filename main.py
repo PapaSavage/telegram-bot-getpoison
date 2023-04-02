df737a6850990b3177ef3384f06f6471c06dbd9c
Danil Falaleev <danilfalaleev2005@yandex.ru>
2023-04-02 20:27:29 +0300

    Calc

--- main.py
+++ main.py
@@ -17,8 +17,17 @@ async def start_command(message: types.Message):
     firstname = message.from_user.first_name
     await bot.send_message(message.from_user.id,
                            text=(
-                               'Добро пожаловать'))
+                               'Добро пожаловать в Get Poison'))
 
+
+@dp.message_handler(commands=["Рассчитать стоимость"])
+async def start_command(message: types.Message):
+    global user_id
+    user_id = message.from_user.id
+    firstname = message.from_user.first_name
+    await bot.send_message(message.from_user.id,
+                           text=(
+                               'Добро пожаловать в Get Poison'))
 
 if __name__ == "__main__":
     executor.start_polling(
