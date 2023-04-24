da45db0f7c0bc5e0afba499a986ec01f71ba211b
Danil Falaleev <danilkafalaleev@gmail.com>
2023-04-24 22:44:15 +0300

    rasschet

--- states.py
+++ states.py
@@ -4,3 +4,8 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 class Start(StatesGroup):
     schet = State()
     smenacursa = State()
+    smenanacenki1 = State()
+    smenanacenki2 = State()
+    smenanacenki3 = State()
+    smenanacenki4 = State()
+    smenanacenki5 = State()
